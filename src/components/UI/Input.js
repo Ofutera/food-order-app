6ed1698eae856e7406fa5db87da84d9063343879
Ondrej Futera <ondra.futera@gmail.com>
2021-08-12 17:50:20 +0200

    Add refs and finish adding cart items

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -2,14 +2,14 @@ import React from "react";
 import classes from "../UI/Input.module.css";
 
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
 
   return (
     <div className={classes.input}>
       <label htmlFor="numberOfItems">{props.label}</label>
-      <input {...props.input}/>
+      <input ref={ref} {...props.input}/>
     </div>
   );
-};
+});
 
 export default Input;
