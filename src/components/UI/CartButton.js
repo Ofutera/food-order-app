95db6b56239a7a43fa7c45d3cbe3436be8a8dc38
Ondrej Futera <ondra.futera@gmail.com>
2021-09-08 15:57:04 +0200

    Add animation to cart button

--- src/components/UI/CartButton.js
+++ src/components/UI/CartButton.js
@@ -1,22 +1,40 @@
-import React, { useContext } from "react";
+import { useContext, useEffect, useState } from 'react';
 
-import classes from "./CartButton.module.css";
-import CartIcon from "../Cart/CartIcon";
-import CartContext from "../../store/cart-context";
+import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
+import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartCtx.items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
-      <span>{props.name}</span>
+      <span>Your Cart</span>
       <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
